2161097d32a2a95852f447d5a339e7576da20319
Ansh-699 <anshtyagi7845@gmail.com>
2025-04-17 16:08:50 +0530

    17-04-2025

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+
+// function demo () {
+//   console.log("This is a demo function.");
+//   console.log(typeof (demo));
+// }
+
+// demo();
+
+//arrow function demo 
+// const arrowDemo = () => {
+//   console.log("This is an arrow function demo.");
+//   console.log(typeof (arrowDemo));
+// }
+// arrowDemo();
+
+// let add = (a, b) => {
+//     return a + b;
+//     }
+
+// console.log(add(5, 10));
+
+
+// let mul = function (){
+//     let c = 5;
+//     let d = 10;
+//     console.log(c * d);
+// }
+// mul();
+
+
+// let div = function (a, b) {
+//     console.log( a / b);
+// }
+// div(10, 5);
+
+// (
+//     function demo() {
+//         console.log("This is a demo function.");
+//     }
+// )();
+
+
+// function add(a, b) {
+//    let sum = a + b;
+//    console.log("Sum is: " + sum);
+//    return function (c) {
+//        console.log("Sum + c is: " + (sum + c));
+//    }
+// }
+// add(5, 10)(15); // Output: Sum is: 15, Sum + c is: 30
+
+
+//callback function
+//  function greet(name) {
+//      console.log("Hello, " + name + "!");
+//  }  
+
+//   greet("John"); 
